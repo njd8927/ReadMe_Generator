329a10b0faf164c6f32c362697379b1f6015e0cd
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-04 16:32:43 -0400

    updated generatemarkdown to not include 2 license sections

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# nick;
-  ## nick;
-  ## nick;
-  ## n;
-  ## n;
-  ### ### License
-    This project is licensed under the GNU License;
-  ### n;
-  ### n;
-  ### Questions;
-  GitHub Repo: n;
-  Email: n;
+# README_Generator;
+### License
+This project is licensed under the MIT License;
+## Description
+## this project is intended to provide a user with a professional README generator to use for their own projects' README;
+## Contents
+## Installation \n Usage \n License \n Contributions \n Tests \n Questions;
+## Installation
+## see github repo for code: github.com/njd8927/read_me_generator;
+## Usage
+## type node index.js in your terminal and answer the corresponding prompts to generate your readme;
+### License
+This project is licensed under the MIT License;
+## Contributions
+## github.com;
+## Tests
+## jest used to test;
+## Questions
+GitHub Repo: njd8927;
+Email: nz@gmail.com;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,15 +2,15 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'Apache') {
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
   } else if (license === 'GNU') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
   } else if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
   } else if (license === 'Mozilla') {
-    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
   } else {
-    return "";
+return "";
   }
 };
 
@@ -18,15 +18,15 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'Apache') {
-    return 'https://opensource.org/licenses/Apache-2.0';
+return 'https://opensource.org/licenses/Apache-2.0';
   } else if (license === 'GNU') {
-    return 'https://www.gnu.org/licenses/gpl-3.0';
+return 'https://www.gnu.org/licenses/gpl-3.0';
   } else if (license === 'MIT') {
-    return 'https://opensource.org/licenses/MIT';
+return 'https://opensource.org/licenses/MIT';
   } else if (license === 'Mozilla') {
-    return 'https://opensource.org/licenses/MPL-2.0';
+return 'https://opensource.org/licenses/MPL-2.0';
   } else {
-    return "";
+return "";
   }
 };
 
@@ -53,16 +53,22 @@ This project is licensed under the Mozilla License`;
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title};
-  ## ${data.description};
-  ## ${data.contents};
-  ## ${data.installation};
-  ## ${data.usage};
-  ${renderLicenseSection(data.license)};
-  ### ${data.contributions};
-  ### ${data.tests};
-  ### Questions;
-  GitHub Repo: ${data.github};
-  Email: ${data.email};
+${renderLicenseSection(data.license)};
+## Description
+## ${data.description};
+## Contents
+## ${data.contents};
+## Installation
+## ${data.installation};
+## Usage
+## ${data.usage};
+## Contributions
+## ${data.contributions};
+## Tests
+## ${data.tests};
+## Questions
+GitHub Repo: ${data.github};
+Email: ${data.email};
 `;
 };
 
